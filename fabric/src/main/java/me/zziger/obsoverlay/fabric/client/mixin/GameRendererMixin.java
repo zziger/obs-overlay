a491431c21e74d533fa49e225d86248cb5a0591e
Artem Dzhemesiuk <programming@zziger.me>
2024-07-22 17:21:18 +0200

    feat: replace screen render redirect mixin with injects to improve mod compatibility

--- fabric/src/main/java/me/zziger/obsoverlay/fabric/client/mixin/GameRendererMixin.java
+++ fabric/src/main/java/me/zziger/obsoverlay/fabric/client/mixin/GameRendererMixin.java
@@ -1,5 +1,6 @@
 package me.zziger.obsoverlay.fabric.client.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import me.zziger.obsoverlay.OBSOverlay;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -8,17 +9,19 @@ import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderWithTooltip(Lnet/minecraft/client/gui/DrawContext;IIF)V", shift = At.Shift.BEFORE))
+    private void beforeScreenRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+        Screen screen = ((GameRenderer) (Object) this).getClient().currentScreen;
+        if (screen != null) OBSOverlay.beforeScreenRender(screen);
+    }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderWithTooltip(Lnet/minecraft/client/gui/DrawContext;IIF)V"))
-    private void renderScreen(Screen instance, DrawContext context, int mouseX, int mouseY, float delta) {
-        OBSOverlay.beforeScreenRender(instance);
-        instance.renderWithTooltip(context, mouseX, mouseY, delta);
-        OBSOverlay.afterScreenRender(instance, context);
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderWithTooltip(Lnet/minecraft/client/gui/DrawContext;IIF)V", shift = At.Shift.AFTER))
+    private void afterScreenRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci, @Local(index = 9) DrawContext context) {
+        Screen screen = ((GameRenderer) (Object) this).getClient().currentScreen;
+        if (screen != null) OBSOverlay.afterScreenRender(screen, context);
     }
 }
--- neoforge/src/main/java/me/zziger/obsoverlay/neoforge/mixin/GameRendererMixin.java
+++ neoforge/src/main/java/me/zziger/obsoverlay/neoforge/mixin/GameRendererMixin.java
@@ -1,19 +1,27 @@
 package me.zziger.obsoverlay.neoforge.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import me.zziger.obsoverlay.OBSOverlay;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/ClientHooks;drawScreen(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/gui/DrawContext;IIF)V"))
-    private void renderScreen(Screen instance, DrawContext context, int mouseX, int mouseY, float delta) {
-        OBSOverlay.beforeScreenRender(instance);
-        instance.renderWithTooltip(context, mouseX, mouseY, delta);
-        OBSOverlay.afterScreenRender(instance, context);
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/ClientHooks;drawScreen(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/gui/DrawContext;IIF)V", shift = At.Shift.BEFORE))
+    private void beforeScreenRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+        Screen screen = ((GameRenderer) (Object) this).getClient().currentScreen;
+        if (screen != null) OBSOverlay.beforeScreenRender(screen);
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/ClientHooks;drawScreen(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/gui/DrawContext;IIF)V", shift = At.Shift.AFTER))
+    private void afterScreenRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci, @Local(index = 9) DrawContext context) {
+        Screen screen = ((GameRenderer) (Object) this).getClient().currentScreen;
+        if (screen != null) OBSOverlay.afterScreenRender(screen, context);
     }
 }
